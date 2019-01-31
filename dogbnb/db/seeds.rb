171ccedbcac3037dcb5fa8325962315264fd5d00
Margaux <45797654+MargVander@users.noreply.github.com>
2019-01-31 16:28:03 +0100

    modif

--- dogbnb/db/seeds.rb
+++ dogbnb/db/seeds.rb
@@ -1,20 +1,20 @@
 require 'faker'
 
-# 10.times do
-# 	city = City.create!(name: Faker::Nation.capital_city)
-# end
+10.times do
+	city = City.create!(name: Faker::Nation.capital_city)
+end
 
-# 30.times do
-# 	dogsitter = Dogsitter.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
-# 	dogsitter.city = City.all.sample
-# 	dogsitter.save
-# end
+30.times do
+	dogsitter = Dogsitter.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+	dogsitter.city = City.all.sample
+	dogsitter.save
+end
 
-# 30.times do
-# 	dog = Dog.new(name: Faker::Pokemon.name)
-# 	dog.city = City.all.sample
-# 	dog.save
-# end
+30.times do
+	dog = Dog.new(name: Faker::Pokemon.name)
+	dog.city = City.all.sample
+	dog.save
+end
 
 50.times do
 	stroll = Stroll.new(date: Faker::Time.backward(14, :all))
